755672eb260f5e43e9ef795189f4161ce91ce51f
Jennifer Luu <JenniferLuu@Jennifers-MacBook-Pro-106.local>
2013-11-04 08:35:55 -0800

    assignment 6

---
+++ assignment6.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	$("#sortMe").sortable();
+	$("#accordion").accordion({
+		collapsible: true
+	});
+	$(".pics").mouseover(function(){
+		$(".pics").animate({
+			opacity: "0"
+		});
+		
+	});
+	var animal = "";
+	function hideAll(){
+
+		$("#cat").hide();
+		$("#chicken").hide();
+		$("#dragon").hide();
+		$("#pony").hide();
+		$("#squash").hide();
+	}
+	hideAll();
+	$("#getAnimal").click(function(){
+		hideAll();
+		animal = $("#pics").val();
+		options = { percent: 100 };
+		if(animal === "cat"){
+			$("#cat").show("fold", options);
+		} else if(animal === "chicken"){
+			$("#chicken").show("fold", options);
+		} else if(animal === "dragon"){
+			$("#dragon").show("fold", options);
+		} else if(animal === "pony"){
+			$("#pony").show("fold", options);
+		} else {
+			$("#squash").show("fold", options);
+		}
+	});
+	var state = true;
+	var clicks = 0;
+	$("#color").click(function(){
+		clicks++;
+		$("#progress").progressbar({
+			value: clicks
+		});
+		if(clicks >= 100){
+			$("#color").animate({
+				backgroundColor: "#FF66FF"
+			}, 225);
+		}
+		if(state){
+			$("#color").animate({
+				backgroundColor: "#FF9933"
+			}, 225);
+			state = !state;
+		} else {
+			$("#color").animate({
+				backgroundColor: "#B0B0B0"
+			}, 225);
+			state = !state;
+		}
+	});
+});
